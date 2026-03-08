030e63c9541e089912e37984a4d923f7e18604e9
s6604062636593 <s6604062636593@email.kmutnb.ac.th>
2026-03-08 17:53:04 +0700

    Add files via upload

---
+++ LAB7-4.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void printHead(int w);
+void printBody(int w, int h);
+
+void printHead(int w){
+    int i;
+    for(i=0;i<w;i++){
+        printf("* ");
+    }
+    printf("\n");
+}
+
+void printBody(int w, int h){
+    int i,j;
+
+    for(i=0;i<h-2;i++){
+        printf("*");
+
+        for(j=0;j<(w*2)-3;j++){
+            printf(" ");
+        }
+
+        printf("*\n");
+    }
+}
+
+int main(){
+    int width,height;
+
+    scanf("%d %d",&width,&height);
+
+    printHead(width);
+    printBody(width,height);
+    printHead(width);
+}
